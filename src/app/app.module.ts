c13f907e3d8460ea76c7a96c9d85f73dc509ddc8
Tejaswini Kintali <tejaswini.kintali@gmail.com>
2019-03-14 14:08:17 -0700

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GroceryComponent } from './app-grocery/app.grocery';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GroceryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
+  exports: [],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, GroceryComponent ]
 })
 export class AppModule { }
